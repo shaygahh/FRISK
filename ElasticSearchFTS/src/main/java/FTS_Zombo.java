ec562700aa488d6fde02c4b8afe586a26888fa41
shaygahh <shaygahh@gmail.com>
2021-03-05 10:11:26 +0200

    Final Frisk

--- ElasticSearchFTS/src/main/java/FTS_Zombo.java
+++ ElasticSearchFTS/src/main/java/FTS_Zombo.java
@@ -1,7 +1,8 @@
 import java.sql.*;
+import java.util.concurrent.TimeUnit;
 
 public class FTS_Zombo {
-    static String execute(String sql) {
+    static void execute(String sql) {
         String url = "jdbc:postgresql://localhost:5432/fakedb";
         String user = "usershay";
         String password = "shaygah14";
@@ -10,25 +11,25 @@ public class FTS_Zombo {
             Connection connection = DriverManager.getConnection(url, user, password);
             Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            String result = "\n RESULTS: \n";
-            while (rs.next()) {
-                result = result + " " + rs.getString(1);
-                result = result + " " + rs.getString(2);
-                result = result + " " + rs.getString(3);
-                result = result + " " + rs.getString(4);
-                result = result + " " + rs.getString(5);
-                result = result + " " + rs.getString(6);
-                result = result + " " + rs.getString(7);
-                result = result + " " + rs.getString(8);
-                result = result + " " + rs.getString(9);
-                result = result + " " + rs.getString(10);
-                result = result + "\n";
-            }
-            return result;
+//            String result = "\n RESULTS: \n";
+//            while (rs.next()) {
+//                result = result + " " + rs.getString(1);
+//                result = result + " " + rs.getString(2);
+//                result = result + " " + rs.getString(3);
+//                result = result + " " + rs.getString(4);
+//                result = result + " " + rs.getString(5);
+//                result = result + " " + rs.getString(6);
+//                result = result + " " + rs.getString(7);
+//                result = result + " " + rs.getString(8);
+//                result = result + " " + rs.getString(9);
+//                result = result + " " + rs.getString(10);
+//                result = result + "\n";
+           // }
+            //return result;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return "NOTHING";
+        //return "NOTHING";
     }
 
     public static void main(String[] args) {
@@ -36,29 +37,29 @@ public class FTS_Zombo {
         long start, normOne, ANDOne, OROne, langOne, highOne, rankOne;
         String result, sql;
 
-        //normal query
-//        start = System.nanoTime();
-//        sql = " SELECT * FROM fakertb WHERE fakertb ==> 'Horror';";
-//        result = execute(sql);
-//        normOne = ((System.nanoTime() - start) / 1_000_000_000 / 60);
+       // normal query
+        start = System.nanoTime();
+        sql = " SELECT * FROM fakertb WHERE fakertb ==> 'Horror';";
+      execute(sql);
+        normOne = TimeUnit.NANOSECONDS.toMillis((System.nanoTime() - start));
 
-//        //AND query
-//        start = System.nanoTime();
-//        sql = " SELECT * FROM fakertb WHERE fakertb ==> 'Horror AND west';";
-//        result = execute(sql);
-//        ANDOne = ((System.nanoTime() - start) / 1_000_000_000);
-//
-//        //OR query
-//        start = System.nanoTime();
-//        sql = " SELECT * FROM fakertb WHERE fakertb ==> 'Horror, Classic';";
-//        result = execute(sql);
-//        OROne = ((System.nanoTime() - start) / 1_000_000_000 / 60);
+        //AND query
+        start = System.nanoTime();
+        sql = " SELECT * FROM fakertb WHERE fakertb ==> 'Horror AND west';";
+   execute(sql);
+        ANDOne = TimeUnit.NANOSECONDS.toMillis((System.nanoTime() - start));
+
+        //OR query
+        start = System.nanoTime();
+        sql = " SELECT * FROM fakertb WHERE fakertb ==> 'Horror, Classic';";
+      execute(sql);
+        OROne = TimeUnit.NANOSECONDS.toMillis((System.nanoTime() - start));
 //
         //lang query
         start = System.nanoTime();
         sql = " SELECT * FROM fakertb WHERE fakertb ==> 'russian AND новгород';";
-        result = execute(sql);
-        langOne = ((System.nanoTime() - start) / 1_000_000_000);
+       execute(sql);
+        langOne = TimeUnit.NANOSECONDS.toMillis((System.nanoTime() - start));
 //
 //        //rank query !!!!
 //        start = System.nanoTime();
@@ -74,9 +75,9 @@ public class FTS_Zombo {
 
         System.out.println("1 MILLION DATASET - ELASTICSEARCH WITH ZOMBODB FTS");
         System.out.println("---------------------------------------");
-      //  System.out.println("Time taken for normal query: " + normOne + " minute(s).");
-      //  System.out.println(result + "Time taken for AND query: " + ANDOne + " seconds(s).");
-       // System.out.println("Time taken for OR query: " + OROne + " minute(s).");
-        System.out.println("Time taken for lang query: " + langOne + " seconds(s).");
+        System.out.println("Time taken for simple query: " + normOne + " millisecond(s).");
+        System.out.println("Time taken for AND query: " + ANDOne + " millisecond(s).");
+        System.out.println("Time taken for OR query: " + OROne + " millisecond(s).");
+        System.out.println("Time taken for lang query: " + langOne + " millisecond(s).");
     }
 }
--- PostgresFTS/src/main/java/FTS.java
+++ PostgresFTS/src/main/java/FTS.java
@@ -1,8 +1,9 @@
 import java.sql.*;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class FTS {
-    static String execute(String sql) {
+    static void execute(String sql) {
         String url = "jdbc:postgresql://localhost:5432/fakedb";
         String user = "usershay";
         String password = "shaygah14";
@@ -11,25 +12,25 @@ public class FTS {
             Connection connection = DriverManager.getConnection(url, user, password);
             Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            String result = "\n RESULTS: \n";
-            while (rs.next()) {
-                result = result + " " + rs.getString(1);
-                result = result + " " + rs.getString(2);
-                result = result + " " + rs.getString(3);
-                result = result + " " + rs.getString(4);
-                result = result + " " + rs.getString(5);
-                result = result + " " + rs.getString(6);
-                result = result + " " + rs.getString(7);
-                result = result + " " + rs.getString(8);
-                result = result + " " + rs.getString(9);
-                result = result + " " + rs.getString(10);
-                result = result + "\n";
-            }
-            return result;
+            //String result = "\n RESULTS: \n";
+//            while (rs.next()) {
+//                result = result + " " + rs.getString(1);
+//                result = result + " " + rs.getString(2);
+//                result = result + " " + rs.getString(3);
+//                result = result + " " + rs.getString(4);
+//                result = result + " " + rs.getString(5);
+//                result = result + " " + rs.getString(6);
+//                result = result + " " + rs.getString(7);
+//                result = result + " " + rs.getString(8);
+//                result = result + " " + rs.getString(9);
+//                result = result + " " + rs.getString(10);
+//                result = result + "\n";
+//            }
+            //return result;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return "NOTHING";
+        //return "NOTHING";
     }
 
     public static void main(String[] args) {
@@ -39,28 +40,28 @@ public class FTS {
         long start, normOne, ANDOne, OROne, langOne, highOne, rankOne;
         String result;
         //normal query
-//        start = System.nanoTime();
-//        sql = " SELECT * FROM fakertb WHERE document @@ to_tsquery('english', 'Horror');";
-//       result = execute(sql);
-//        normOne = ((System.nanoTime() - start) / 1_000_000_000 / 60);
+        start = System.nanoTime();
+        sql = " SELECT * FROM fakertb WHERE document @@ to_tsquery('english', 'Horror');";
+       execute(sql);
+        normOne = TimeUnit.NANOSECONDS.toMillis((System.nanoTime() - start));
 
-////        //query with AND
-//        start = System.nanoTime();
-//        sql = " SELECT * FROM fakertb WHERE document @@ to_tsquery('english', 'Horror') AND document @@ to_tsquery('english', 'west');";
-//        result = execute(sql);
-//        ANDOne = ((System.nanoTime() - start) / 1_000_000_000);
+//        //query with AND
+        start = System.nanoTime();
+        sql = " SELECT * FROM fakertb WHERE document @@ to_tsquery('english', 'Horror') AND document @@ to_tsquery('english', 'west');";
+        execute(sql);
+        ANDOne = TimeUnit.NANOSECONDS.toMillis((System.nanoTime() - start));
+
+        //query with OR
+        start = System.nanoTime();
+        sql = " SELECT * FROM fakertb WHERE document @@ to_tsquery('english', 'Horror') OR document @@ to_tsquery('english', 'Classic');";
+        execute(sql);
+        OROne = TimeUnit.NANOSECONDS.toMillis((System.nanoTime() - start));
 //
-//        //query with OR
-//        start = System.nanoTime();
-//        sql = " SELECT * FROM fakertb WHERE document @@ to_tsquery('english', 'Horror') OR document @@ to_tsquery('english', 'Classic');";
-//        execute(sql);
-//        OROne = ((System.nanoTime() - start) / 1_000_000_000) / 60;
-////
-//        //query with LANG
-//        start = System.nanoTime();
-//        sql = " SELECT * FROM fakertb WHERE document @@ to_tsquery('russian', 'новгород');";
-//        result = execute(sql);
-//        langOne = ((System.nanoTime() - start) / 1_000_000_000) ;
+        //query with LANG
+        start = System.nanoTime();
+        sql = " SELECT * FROM fakertb WHERE document @@ to_tsquery('russian', 'новгород');";
+        execute(sql);
+        langOne = TimeUnit.NANOSECONDS.toMillis((System.nanoTime() - start));
 ////
 //        //query with ranks
 //        start = System.nanoTime();
@@ -72,23 +73,23 @@ public class FTS {
 //        execute(sql);
 //        rankOne = ((System.nanoTime() - start) / 1_000_000_000) / 60;
 
-        //query with highlights
-        start = System.nanoTime();
-        sql = " SELECT * FROM fakertb WHERE ts_headline('document', to_tsquery('english', 'Essay'))" ;
-                  //"FROM fakertb" ;
-//                  "WHERE document @@ to_tsquery('english', 'Essay')" +
-//                    "ORDER BY id;";
-        result = execute(sql);
-        highOne = ((System.nanoTime() - start) / 1_000_000_000) / 60;
+//        //query with highlights
+//        start = System.nanoTime();
+//        sql = " SELECT * FROM fakertb WHERE ts_headline('document', to_tsquery('english', 'Essay'))" ;
+//                  //"FROM fakertb" ;
+////                  "WHERE document @@ to_tsquery('english', 'Essay')" +
+////                    "ORDER BY id;";
+//        result = execute(sql);
+//        highOne = ((System.nanoTime() - start) / 1_000_000_000) / 60;
 
         //print results
         System.out.println("1 MILLION DATASET - POSTGRESQL FTS");
         System.out.println("---------------------------------------");
-      //  System.out.println("Time taken for normal query: " + normOne + " minute(s).");
-      // System.out.println("Time taken for AND query: " + ANDOne + " second(s).");
-       // System.out.println("Time taken for OR query: " + OROne + " minutes().");
-       //System.out.println( "Time taken for language query: " + langOne + " seconds.");
-         System.out.println(result + "Time taken for highlight query: " + highOne + " minutes." + result);
+        System.out.println("Time taken for simple query: " + normOne + " millisecond(s).");
+       System.out.println("Time taken for AND query: " + ANDOne + " millisecond(s).");
+        System.out.println("Time taken for OR query: " + OROne + " millisecond(s).");
+       System.out.println( "Time taken for language query: " + langOne + " millisecond(s).");
+        // System.out.println(result + "Time taken for highlight query: " + highOne + " minutes." + result);
 //        System.out.println("Time taken for ranked normal query: " + rankOne + " minutes.");
     }
 }
